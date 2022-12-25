723d36fcb9c77d33a52b2ef6700bd0e56a56d071
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-12-25 15:20:13 +0530

    feat: added zoomable world map

--- src/components/Maps/Map.tsx
+++ src/components/Maps/Map.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
+import MouseTooltip from "react-sticky-mouse-tooltip";
 
 export const Map = () => {
-  return <div>Map</div>;
+  const [tooltipText, setTooltipText] = useState<string>("");
+  return (
+    <div data-tip="">
+      <ComposableMap>
+        <ZoomableGroup>
+          <Geographies geography="https://world-map-backend.sen442b.repl.co/features">
+            {({ geographies }) => {
+              return geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  style={{
+                    default: {
+                      fill: "#D6D6DA",
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#F53",
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none",
+                    },
+                  }}
+                  onMouseEnter={() => setTooltipText(geo.properties.name)}
+                  onMouseLeave={() => setTooltipText("")}
+                />
+              ));
+            }}
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+      {tooltipText && (
+        <MouseTooltip visible={true} offsetX={25} offsetY={15}>
+          <span>{tooltipText}</span>
+        </MouseTooltip>
+      )}
+    </div>
+  );
 };
