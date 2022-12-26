c55d6237ca2ca0c409ec4fc55173a903aae1873d
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-12-26 14:59:53 +0530

    feat: added markers

--- src/components/Maps/Map.tsx
+++ src/components/Maps/Map.tsx
@@ -1,14 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   ComposableMap,
   Geographies,
   Geography,
+  Marker,
   ZoomableGroup,
 } from "react-simple-maps";
 import MouseTooltip from "react-sticky-mouse-tooltip";
+import { csv } from "d3-fetch";
+import { scaleLinear } from "d3";
 
+type CountryDataObj = {
+  "Country or area": string;
+
+  "Internet users": string;
+
+  Percentage: string;
+
+  Subregion: string;
+
+  latitude: string;
+  longitude: string;
+};
 export const Map = () => {
   const [tooltipText, setTooltipText] = useState<string>("");
+  const [maxValue, setMaxValue] = useState<number>(0);
+  const [countriesData, setCountriesData] = useState<CountryDataObj[] | any>(
+    []
+  );
+  useEffect(() => {
+    (async () => {
+      const countries = await csv(
+        "./List_of_countries_by_number_of_Internet_users_3.csv"
+      );
+      const filteredCountries = countries.filter(
+        (country) => country.Percentage
+      );
+      const sortedCountries = [...filteredCountries].sort(
+        (a, b) => Number(b.Percentage) - Number(a.Percentage)
+      );
+      console.log(sortedCountries);
+
+      setMaxValue(Number(sortedCountries[0].Percentage));
+      setCountriesData(sortedCountries);
+    })();
+  }, []);
+  const percentScale = useMemo(
+    () => scaleLinear().domain([0, maxValue]).range([0, 9]),
+    [maxValue]
+  );
   return (
     <div data-tip="">
       <ComposableMap>
@@ -39,6 +79,22 @@ export const Map = () => {
               ));
             }}
           </Geographies>
+          {countriesData.map((country: CountryDataObj) => {
+            const { latitude, longitude, Percentage: percentage } = country;
+
+            return (
+              <Marker
+                key={crypto.randomUUID()}
+                coordinates={[Number(longitude), Number(latitude)]}
+              >
+                <circle
+                  fill="rgba(43, 136, 229, 0.776)"
+                  stroke="rgba(255, 255, 255, 0.833)"
+                  r={percentScale(Number(percentage))}
+                />
+              </Marker>
+            );
+          })}
         </ZoomableGroup>
       </ComposableMap>
       {tooltipText && (
